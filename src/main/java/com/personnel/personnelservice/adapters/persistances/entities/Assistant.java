39838150b7c97dbb5d197ef10da1faf96188bca8
demsscash <demsscash@gmail.com>
2025-04-27 23:22:38 +0000

    v3

---
+++ src/main/java/com/personnel/personnelservice/adapters/persistances/entities/Assistant.java
@@ -0,0 +1,15 @@
+package com.personnel.personnelservice.adapters.persistances.entities;
+
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Assistant extends User {
+    // Assistant-specific fields, if any
+}
---
+++ src/main/java/com/personnel/personnelservice/adapters/persistances/entities/Medecin.java
@@ -0,0 +1,14 @@
+package com.personnel.personnelservice.adapters.persistances.entities;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Medecin extends User {
+    // Medecin-specific fields, if any
+}
